99e7da44fdb2c625a0c98cf94a174d6d1db81d4e
calidron <calidron@duck.com>
2026-07-11 15:09:52 -0500

    Initial Commit

---
+++ timeshtTests/timeshtTests.swift
@@ -0,0 +1,17 @@
+//
+//  timeshtTests.swift
+//  timeshtTests
+//
+//  Created by Andrew on 7/11/26.
+//
+
+import Testing
+@testable import timesht
+
+struct timeshtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
